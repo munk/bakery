6aac4cd75a1fbb3301ba6b9171cd85e6b74cbaec
John Downs <john.downs@acm.org>
2017-01-11 21:38:58 -0500

    refactor treat detail

--- src/cljs/bakery/views.cljs
+++ src/cljs/bakery/views.cljs
@@ -1,30 +1,34 @@
 (ns bakery.views
     (:require [re-frame.core :as re-frame]))
 
-(defn treat-detail [id]
+(defn treat-thumbnail [url]
+  [:img {:src url
+         :style {:float :left
+                 :height "100px"
+                 :width "100px"}}])
+
+(defn treat-detail [name price bulkPricing]
+  [:div {:style {:padding-left 110}}
+   [:p {:style {:padding-top 10}} name]
+   [:p "$" (.toFixed price 2)
+    (when bulkPricing
+      (let [{:keys [totalPrice amount]} bulkPricing]
+        (str " or " amount " for $" (.toFixed totalPrice 2))))]
+   [:button  {:style {:position :absolute
+                      :bottom 0}}
+    "Add to Cart"]])
+
+(defn treat-component [id]
   (let [treat (re-frame/subscribe [:treats 1])]
     (fn []
-      (println @treat)
-      [:div {:id "treat-detail"
-             :text-align :center
-             :style {:height "100px"
-                     :width "250px"
-                     :position :relative}}
-       [:img {:src (:imageURL @treat)
-              :style {:float :left
-                      :height "100px"
-                      :width "100px"}}]
-       [:div {:style {:padding-left 110}}
-        [:p (:name @treat)]
-        [:p "$" (.toFixed (:price @treat) 2)
-         (when (:bulkPricing @treat)
-           (let [price (get-in @treat [:bulkPricing :totalPrice])
-                 amount (get-in @treat [:bulkPricing :amount])]
-             (str " or " amount " for $" (.toFixed price 2))))]
-        [:button  {:style {:position :absolute
-                           :bottom 0}}
-         "Add to Cart"]]])))
+      (let [{:keys [name price imageURL bulkPricing]} @treat]
+        [:div {:id "treat-detail"
+               :style {:height "100px"
+                       :width "250px"
+                       :position :relative}}
+         [treat-thumbnail imageURL]
+         [treat-detail name price bulkPricing]]))))
 
 (defn main-panel []
   (fn []
-    [treat-detail 1]))
+    [treat-component 1]))
